3d938ed5fbb8afaa9bbd6967f8d8de11569598ec
DTk8ch <dtkach@kse.org.ua>
2024-11-24 20:43:28 +0200

    improved validation erors

--- validation.py
+++ validation.py
@@ -13,7 +13,7 @@ def validation_country(country):
             alp3.append(guby[1].upper())
             alp2.append(guby[2][:-1].upper())
     if country not in full and country not in alp3 and country not in alp2:
-        print('Such country is not included in the data set.')
+        print("Entered country doesn't exist or is not included in the data set. Please, try again.")
     else:
         if len(country)==2:
             result = alp3[alp2.index(country)]
@@ -31,7 +31,9 @@ def year_validation(year):
             guby = guby.split('\t')
             years.add(guby[9])
         if str(year) not in years:
-            print('Such year is not included in the data set.')
+            years = sorted(list(years))
+            print(f"The Olympics weren't held this year. Try one of the following:\n", *years)
+
             return False
         else:
             return True
